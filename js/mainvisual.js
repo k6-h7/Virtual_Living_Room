b9b0c8eba74d234428062326c0be8df6fcaac16d
k6-h7 <131327489+k6-h7@users.noreply.github.com>
2024-05-15 14:49:39 +0900

    Add files via upload

--- js/mainvisual.js
+++ js/mainvisual.js
@@ -1,26 +1,10 @@
-function change_living(){
-    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/living.jpg")';
-}
-function change_sea(){
-    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/sea.jpg")';
-}
-function change_forest(){
-    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/forest.jpg")';
-}
-function change_green(){
-    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/green.jpg")';
-}
-function change_flower1(){
-    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/flower1.jpg")';
-}
-function change_flower2(){
-    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/flower2.jpg")';
-}
-function change_waterdrop(){
-    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/waterdrop.jpg")';
-}
-/*
-function chenge_random(){
-    document.querySelector('.mainvisual').style.backgroundImage = 
-}
-*/
+const buttons = document.querySelectorAll('.button');
+const mainvisual = document.querySelector('.mainvisual');
+
+// サムネイル画像と同じ画像をメインビジュアルに表示する
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    const src = button.getAttribute('src');
+    mainvisual.style.backgroundImage = 'url(' + src + ')';
+  });
+});
