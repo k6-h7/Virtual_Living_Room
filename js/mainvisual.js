d2f4a69b712b7af25781dd871f8d0683598a7d5c
k6-h7 <131327489+k6-h7@users.noreply.github.com>
2024-05-15 13:37:46 +0900

    Add files via upload

---
+++ js/mainvisual.js
@@ -0,0 +1,26 @@
+function change_living(){
+    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/living.jpg")';
+}
+function change_sea(){
+    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/sea.jpg")';
+}
+function change_forest(){
+    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/forest.jpg")';
+}
+function change_green(){
+    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/green.jpg")';
+}
+function change_flower1(){
+    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/flower1.jpg")';
+}
+function change_flower2(){
+    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/flower2.jpg")';
+}
+function change_waterdrop(){
+    document.querySelector('.mainvisual').style.backgroundImage = 'url("img/waterdrop.jpg")';
+}
+/*
+function chenge_random(){
+    document.querySelector('.mainvisual').style.backgroundImage = 
+}
+*/
