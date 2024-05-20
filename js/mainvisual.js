8d36b1953cda08a3428811a6926ba028a7339c1b
k6-h7 <131327489+k6-h7@users.noreply.github.com>
2024-05-20 11:20:03 +0900

    Add files via upload

--- js/mainvisual.js
+++ js/mainvisual.js
@@ -8,3 +8,16 @@ buttons.forEach(button => {
     mainvisual.style.backgroundImage = 'url(' + src + ')';
   });
 });
+
+// ランダム部分
+var images = [ 'url(img/living.jpg)', 'url(img/sea.jpg)', 'url(img/forest.jpg)', 'url(img/green.jpg)', 'url(img/flower1.jpg)', 'url(img/flower2.jpg)', 'url(img/waterdrop.jpg)' ];  // ランダム表示させたい画像のパス
+
+$(function(){
+  var backgroundRandom = function(){
+    $('.right-navi li:last-child')
+      var number = Math.floor(Math.random() * images.length); // 画像分の数値を算出 
+      var selectedImg = images[number]; // 算出された数値を元に、1行目の配列から取り出す
+      $('.mainvisual').css('background-image',selectedImg); // cssにランダムに選ばれた画像を背景設定する
+  };
+  setInterval(backgroundRandom, 1000);  // 1000msごとにランダム切り替えを繰り返す
+});
